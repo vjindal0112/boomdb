c5a96e9b8609ba064a4198969f8d83ec93382507
Varun Jindal <vjindal0112@gmail.com>
2023-06-18 17:31:19 -0400

    main: add repl infinite loop Additionally, added a > to make it look like a repl.
    Potentially consider allowing for an "exit" command in the future.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod common;
 pub mod utils;
 
 use std::env;
+use std::io::{self, Write};
 use std::path::PathBuf;
 
 use sqlparser::ast::SetExpr;
@@ -14,38 +15,49 @@ fn main() {
     let mut data_base_path: PathBuf = env::current_dir().expect("Failed to get current directory");
     data_base_path.push("data");
     let mut line = String::new();
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
-    let sql = line.trim_end();
-    println!("SQL , {}", line);
-    println!("no of bytes read , {}", b1);
+    loop {
+        println!("");
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        let sql = line.trim_end();
+        println!("no of bytes read , {}", b1);
 
-    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+        let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
 
-    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+        let ast = Parser::parse_sql(&dialect, sql).unwrap();
 
-    for statement in ast {
-        match statement {
-            Query(query) => {
-                commands::select(*query, &data_base_path);
-            }
-            CreateTable { name, columns, .. } => {
-                commands::create_table(name, columns, &data_base_path);
-            }
-            Insert {
-                table_name,
-                columns,
-                source,
-                ..
-            } => match *source.body {
-                SetExpr::Values(values) => {
-                    commands::insert(table_name, columns, values.rows[0].clone(), &data_base_path);
+        for statement in ast {
+            match statement {
+                Query(query) => {
+                    commands::select(*query, &data_base_path);
+                }
+                CreateTable { name, columns, .. } => {
+                    commands::create_table(name, columns, &data_base_path);
+                }
+                Insert {
+                    table_name,
+                    columns,
+                    source,
+                    ..
+                } => {
+                    match *source.body {
+                        SetExpr::Values(values) => {
+                            commands::insert(
+                                table_name,
+                                columns,
+                                values.rows[0].clone(),
+                                &data_base_path,
+                            );
+                        }
+                        _ => {
+                            panic!("There shouldn't be something other than values in an insert statement");
+                        }
+                    }
                 }
                 _ => {
-                    panic!("There shouldn't be something other than values in an insert statement");
+                    panic!("Not supported yet");
                 }
-            },
-            _ => {
-                panic!("Not supported yet");
             }
         }
     }
